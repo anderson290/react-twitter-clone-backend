012d196841e5d30ae45a74b40ab924aa677ce543
Anderson290 <anderson.nunes@fcamara.com.br>
2020-03-21 10:07:50 -0300

    backend socketio twitter

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+mongoose.connect('mongodb://goweek:goweek123@ds141043.mlab.com:41043/ominstack-tweeter',
+{
+    useNewUrlParser: true
+}
+);
+
+
+app.use(cors());
+
+app.use((req, res, next)=>{
+    req.io = io;
+
+    return next();
+})
+
+app.use(express.json());
+
+app.use(require('./routes'));
+
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
